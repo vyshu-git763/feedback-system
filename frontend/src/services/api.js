c409fe8c5bc1cd3b5405794becfd97a5344c633f
Vyshnavi <vyshnavik429@gmail.com>
2025-06-27 22:16:10 +0530

    Update API base URL to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://feedback-frontend-skur.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
